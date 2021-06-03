81d3fb64501cbacd5359570ba29520f2e0b38578
yuxizhe <yuxizhe2008@gmail.com>
2021-06-04 01:33:22 +0800

    WIP: chat room

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const Koa = require("koa");
 const fs = require("fs");
 const Router = require("koa-router");
-const static = require('koa-static');
+const static = require("koa-static");
 
 const app = new Koa();
 const server = require("http").createServer(app.callback());
@@ -9,7 +9,7 @@ const SocketIO = require("socket.io");
 
 const io = SocketIO(server, { origins: "*:*" });
 
-app.use(static('public'));
+app.use(static("public"));
 
 // 路由
 // const router = new Router();
@@ -18,9 +18,36 @@ app.use(static('public'));
 // app.use(router.routes());
 
 // socket连接
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  console.log("username", username);
+  socket.username = username;
+  next();
+});
+
+/**
+ * 获取当前所有用户
+ * @returns users 用户列表
+ */
+function getUsers() {
+  const users = [];
+  for (let [id, socket] of io.of("/").sockets) {
+    users.push({
+      userID: id,
+      username: socket.username,
+    });
+  }
+  return users;
+}
+
 io.on("connect", (socket) => {
   console.log("connect");
   socket.emit("an event", { message: "connected" });
+
+  // 发送用户列表
+  let users = getUsers();
+  io.emit("users", users);
 
   socket.on("chat message", (msg) => {
     console.log(`message: ${msg}`);
@@ -31,8 +58,11 @@ io.on("connect", (socket) => {
     console.log(`subscribe: ${msg}`);
     io.emit("subscribe", msg);
   });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
+    users = getUsers();
+    io.emit("users", users);
   });
 });
 
